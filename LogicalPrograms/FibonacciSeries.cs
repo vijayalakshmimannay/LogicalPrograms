80d48714fbb6bb02e50561afdd5f50acadc153af
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-30 09:25:19 +0530

    [vijayalakshmi] fibonacci series of a given number

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, num3, value;
+            Console.WriteLine("Enter a number");
+            value = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + " " + num2 + " ");
+            for (int i = 2; i < value; ++i)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
+
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+LogicalPrograms.FibonacciSeries fibonacci = new LogicalPrograms.FibonacciSeries();
+fibonacci.Fibonacci();
